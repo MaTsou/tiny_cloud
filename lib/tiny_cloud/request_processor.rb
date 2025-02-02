d02a934fd6c58009c2b93d2755ec699c0216a60a
MaTsou <contact@logicore.fr>
2025-02-02 16:29:08 +0100

    Request object no more needed.

--- lib/tiny_cloud/request_processor.rb
+++ lib/tiny_cloud/request_processor.rb
@@ -2,14 +2,34 @@ module TinyCloud
   class RequestProcessor
     attr_accessor :http_client, :request_formatter
 
-    def initialize( http_client: TinyCloud::Excon::HttpClient.new )
+    def initialize(
+      http_client: TinyCloud::Excon::HttpClient.new,
+      request_formatter: Struct.new( :url, :method, :options )
+    )
       @http_client = http_client
-      @request_formatter = TinyCloud::Request.new
+      @request_formatter = request_formatter
     end
 
     def call( request )
-      http_client.call( request_formatter.call request )
+      http_client.call( formatted_request request )
     end
 
+    private
+
+    def formatted_request( request )
+      request_formatter.new(
+        get_url( request ),
+        request[:method],
+        request[:options]
+      )
+    end
+
+    def get_url( request )
+      join_paths request[:url], request[:path]
+    end
+
+    def join_paths( *paths )
+      paths.compact.join( '/' )
+    end
   end
 end
