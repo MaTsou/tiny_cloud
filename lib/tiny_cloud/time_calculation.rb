e5e1a75d634f70e52e3acc0867dfa1a163216347
MaTsou <contact@logicore.fr>
2025-02-02 14:09:44 +0100

    Improve functionalities.

--- lib/tiny_cloud/time_calculation.rb
+++ lib/tiny_cloud/time_calculation.rb
@@ -3,6 +3,7 @@ module TinyCloud
     SECONDS_PER_HOUR = 3600
     SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR
     SECONDS_PER_WEEK = 7 * SECONDS_PER_DAY
+    SECONDS_PER_YEAR = 365.25 * SECONDS_PER_DAY
 
     def now
       Time.now
@@ -12,10 +13,12 @@ module TinyCloud
       now + convert_in_seconds( { days: 1 } )
     end
 
-    # args is { weeks: w, days: d, hours: h }
+    # args is { year: y, weeks: w, days: d, hours: h }
     def convert_in_seconds( hash )
       hash.reduce( 0 ) do |seconds, delay|
         case [delay].to_h
+        in years:
+          seconds += SECONDS_PER_YEAR * years
         in weeks:
           seconds += SECONDS_PER_WEEK * weeks
         in days:
