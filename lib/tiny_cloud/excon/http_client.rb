413fed36dc35e4fe283e1f9d57afdcc5ba2f3718
MaTsou <contact@logicore.fr>
2025-04-11 18:46:33 +0200

    rubocop enjoys excon module code

--- lib/tiny_cloud/excon/http_client.rb
+++ lib/tiny_cloud/excon/http_client.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 require 'excon'
 
 module TinyCloud
   module Excon
+    # This is the http client
     class HttpClient
       attr_reader :response_formatter
 
-      def initialize( formatter = TinyCloud::Excon::ResponseFormatter.new )
+      def initialize(formatter = TinyCloud::Excon::ResponseFormatter.new)
         @response_formatter = formatter
       end
 
-      def call( request )
+      def call(request)
         response_formatter.call(
           ::Excon
-          .new( request.url )
-          .send( request.method, **request.options )
+          .new(request.url)
+          .send(request.http_method, **request.http_options)
         )
       end
     end
--- lib/tiny_cloud/excon/response_formatter.rb
+++ lib/tiny_cloud/excon/response_formatter.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 module TinyCloud
   module Excon
+    # this is http response formatter
     class ResponseFormatter
-      def call( response )
-        { status_type( response.status ) => response }
+      def call(response)
+        { status_type(response.status).to_sym => response }
       end
 
       private
 
-      def status_type( status )
-        "status#{status.to_i / 100}xx".to_sym
+      def status_type(status)
+        "status#{status.to_i / 100}xx"
       end
     end
   end
