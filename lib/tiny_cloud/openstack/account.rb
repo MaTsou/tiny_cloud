87f470dedb5a7c5078b7676451fecc06be5fdefa
MaTsou <contact@logicore.fr>
2025-04-11 19:08:37 +0200

    rubocop enjoys openstack account and configuration

--- lib/tiny_cloud/openstack/account.rb
+++ lib/tiny_cloud/openstack/account.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 module TinyCloud
   module Openstack
+    # account class
     class Account
       attr_reader :temp_url_manager, :auth_manager, :action_manager
 
-      def initialize( request_processor = TinyCloud::RequestProcessor.new )
+      def initialize(request_processor = TinyCloud::RequestProcessor.new)
         yield configuration
-        @action_manager = Openstack::ActionManager.new( self, request_processor )
+        @action_manager = Openstack::ActionManager.new(self, request_processor)
 
-        @temp_url_manager = Openstack::TempUrlManager.new( configuration )
-        @auth_manager = Openstack::AuthManager.new( configuration )
+        @temp_url_manager = Openstack::TempUrlManager.new(configuration)
+        @auth_manager = Openstack::AuthManager.new(configuration)
       end
 
       def configuration
         @configuration ||= Openstack::Configuration.new
       end
 
-      def call( action, context )
-        @action_manager.call( action, context )
+      def call(action, context)
+        @action_manager.call(action, context)
       end
-
     end
   end
 end
--- lib/tiny_cloud/openstack/configuration.rb
+++ lib/tiny_cloud/openstack/configuration.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 module TinyCloud
   module Openstack
+    # openstack configuration class
     class Configuration
-      attr_accessor :project_name, :project_domain_name, :project_id,
+      attr_accessor(
+        :project_name, :project_domain_name, :project_id,
         :root_url, :region_name, :api_version,
         :user_name, :user_domain_name, :auth_url, :password,
         :temp_url_key_reset_after, :temp_url_default_life_time
+      )
     end
   end
 end
