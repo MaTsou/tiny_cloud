eca1c1ae70835307255308b682b9e87d634c7a52
MaTsou <contact@logicore.fr>
2025-04-11 18:47:18 +0200

    rubocop enjoys time_calculation code

--- lib/tiny_cloud/time_calculation.rb
+++ lib/tiny_cloud/time_calculation.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module TinyCloud
+  # time calculation module
   module TimeCalculation
     SECONDS_PER_HOUR = 3600
     SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR
@@ -10,26 +13,29 @@ module TinyCloud
     end
 
     def tomorrow
-      now + convert_in_seconds( { days: 1 } )
+      now + convert_in_seconds({ days: 1 })
     end
 
     # args is { year: y, weeks: w, days: d, hours: h }
-    def convert_in_seconds( hash )
-      hash.reduce( 0 ) do |seconds, delay|
-        case [delay].to_h
-        in years:
-          seconds += SECONDS_PER_YEAR * years
-        in weeks:
-          seconds += SECONDS_PER_WEEK * weeks
-        in days:
-          seconds += SECONDS_PER_DAY * days
-        in hours:
-          seconds += SECONDS_PER_HOUR * hours
-        else
-          seconds
-        end
+    def convert_in_seconds(hash)
+      hash.reduce(0) do |seconds, delay|
+        seconds + to_seconds([delay].to_h)
       end
     end
 
+    private
+
+    def to_seconds(delay)
+      case delay
+      in years:
+        SECONDS_PER_YEAR * years
+      in weeks:
+        SECONDS_PER_WEEK * weeks
+      in days:
+        SECONDS_PER_DAY * days
+      in hours:
+        SECONDS_PER_HOUR * hours
+      else 0 end
+    end
   end
 end
